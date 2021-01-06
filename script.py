58dc60662fa40cf1419e8b415ca9f76570e600bb
dorin.miron <dorin.miron@metrosystems.net>
2021-01-06 17:07:52 +0200

    Added levenshtein distance

--- script.py
+++ script.py
@@ -1,38 +1,46 @@
-import cv2 
+import cv2
 import pytesseract
 from nltk.corpus import words
 from pyspark import SparkContext, SparkConf
 import random
 import numpy as np
 
-images=['1', '2', '3', '4']
-sc = SparkContext("local", "PySpark Word Count Exmaple")
+images = ['1', '2', '3', '4']
 
 
-def check_for_word(text):
-    s = text.split(' ')
-    for word in s:
-        if word not in words.words():
-            s.remove(word)
-    return ' '.join(s)
+def levenshtein(seq1, seq2):
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = np.zeros((size_x, size_y))
+    for x in range(size_x):
+        matrix[x, 0] = x
+    for y in range(size_y):
+        matrix[0, y] = y
 
-
-def word_count(text):
-    words = sc.parallelize(text.split(" "))
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b).sortBy(lambda a: a[1])
-    return [''.join(str(i)) for i in wordCounts.collect()]
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if seq1[x - 1] == seq2[y - 1]:
+                matrix[x, y] = min(
+                    matrix[x - 1, y] + 1,
+                    matrix[x - 1, y - 1],
+                    matrix[x, y - 1] + 1
+                )
+            else:
+                matrix[x, y] = min(
+                    matrix[x - 1, y] + 1,
+                    matrix[x - 1, y - 1] + 1,
+                    matrix[x, y - 1] + 1
+                )
+    return matrix[size_x - 1, size_y - 1]
 
 
 def extract_text_and_save(img, img_nr, flow_type):
     custom_config = r'--oem 3 --psm 6'
     textFromImage = pytesseract.image_to_string(img, config=custom_config)
-    txt = word_count(check_for_word(textFromImage))
-    txt.reverse()
-    file = open(f'data/{img_nr}/{flow_type}.txt', 'w')
-    for line in txt:
-        file.writelines(line)
-        file.write("\n")
-    file.close()
+    f = open(f'data/{img_nr}/{flow_type}.txt', 'w')
+    f.write(textFromImage)
+    f.close()
+    return textFromImage
 
 
 def read_image(img_path):
@@ -53,6 +61,7 @@ def add_noise(img):
         x_coord = random.randint(0, col - 1)
         img[y_coord][x_coord] = 0
     return img
+
 
 def normal(img):
     return img
@@ -95,7 +104,7 @@ def resize(img):
 
 def average_filters(img):
     dim = 2
-    kernel = np.ones((dim, dim), np.float32) / dim*dim
+    kernel = np.ones((dim, dim), np.float32) / dim * dim
     dst = cv2.filter2D(img, -1, kernel)
     return dst
 
@@ -107,17 +116,11 @@ def sharpen(img):
 
 
 def sharpen_laplacian(img):
-    filter = np.array([[0,0,-1,0,0],[0,-1,-2,-1,0],[-1,-2,16,-2,-1],[0,-1,-2,-1,0],[0,0,-1,0,0]])
-    mexican_hat_img1=cv2.filter2D(img,-1,filter)
+    filter = np.array(
+        [[0, 0, -1, 0, 0], [0, -1, -2, -1, 0], [-1, -2, 16, -2, -1], [0, -1, -2, -1, 0], [0, 0, -1, 0, 0]])
+    mexican_hat_img1 = cv2.filter2D(img, -1, filter)
     return mexican_hat_img1
 
-
-def sharpen_flow(image_numbers):
-    for img_nr in image_numbers:
-        img = read_image(f'data/{img_nr}/{img_nr}.png')
-        img = sharpen(img)
-        cv2.imwrite(f'data/{img_nr}/-sharpen.png', img)
-        extract_text_and_save(img, img_nr, "sharpen")
 
 def erosion(img):
     kernel = np.ones((5, 5), np.uint8)
@@ -136,6 +139,7 @@ def opening(img):
     opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
     return opening
 
+
 def closing(img):
     kernel = np.ones((5, 5), np.uint8)
     closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
@@ -148,13 +152,12 @@ def morphological_gradient(img):
     return gradient
 
 
-def generic_flow(image_numbers, flow_type):
+def generic_flow(img_nr, flow_type):
     flow_name = flow_type.__name__
-    for img_nr in image_numbers:
-        img = read_image(f'data/{img_nr}/{img_nr}.png')
-        img = flow_type(img)
-        cv2.imwrite(f'data/{img_nr}/{flow_name}.png', img)
-        extract_text_and_save(img, img_nr, f"{flow_name}")
+    img = read_image(f'data/{img_nr}/{img_nr}.png')
+    img = flow_type(img)
+    cv2.imwrite(f'data/{img_nr}/{flow_name}.png', img)
+    return extract_text_and_save(img, img_nr, f"{flow_name}")
 
 
 flows = [normal,
@@ -164,5 +167,11 @@ flows = [normal,
          average_filters,
          sharpen, sharpen_laplacian, erosion, dilation, opening, closing, morphological_gradient]
 
-for flow in flows:
-    generic_flow(images, flow)
+for img_nr in images:
+    normal_img = read_image(f'data/{img_nr}/{img_nr}.png')
+    normal_txt = extract_text_and_save(normal_img, img_nr, "normal")
+    for flow in flows:
+        modified_text = generic_flow(img_nr, flow)
+        distance = levenshtein(normal_txt, modified_text)
+        print(f'distance levenshtein between initial image {img_nr} and image after {flow.__name__} is {distance}')
+    print("\n")
